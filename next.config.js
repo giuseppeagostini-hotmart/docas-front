b5e5ac997f52784367d04bcf32d02a00434910fe
Giuseppe Fantoni <109975226+giuseppeagostini-hotmart@users.noreply.github.com>
2025-05-26 21:03:23 -0300

    fic

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NODE_ENV === 'production' ? '/docas-front' : '';
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '/docas-front' : '';
 
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
   basePath,
+  assetPrefix: basePath, // ⭐ Isso é crucial para o favicon
+  
   env: {
     NEXT_PUBLIC_BASE_PATH: basePath,
-  },
+  }
 };
 
 module.exports = nextConfig;
