babf1ba6b8ff13dccf0e8253f2fa4abce9cb1f1c
Giuseppe Fantoni <109975226+giuseppeagostini-hotmart@users.noreply.github.com>
2025-11-09 16:13:41 -0300

    fix: fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,10 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {  
+}>) {
+  const isProd = process.env.NODE_ENV === 'production';
+  const basePath = isProd ? '/docas-front' : '';
+  
   return (
     <html lang="pt-BR" className="scroll-smooth">
       <head>
